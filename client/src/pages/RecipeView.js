791a58ab571ab8b11de201974696f386e02a4a38
Parker Mathis <parkerjmathis@gmail.com>
2023-05-21 10:47:34 -0700

    Populating data on recipe view page

--- client/src/pages/RecipeView.js
+++ client/src/pages/RecipeView.js
@@ -4,26 +4,59 @@ import axios from "axios";
 
 export default function RecipeView({ recipeId }) {
   const [recipeData, setRecipeData] = useState([]);
+  const [ingredientData, setIngredientData] = useState([]);
+  const [directionData, setDirectionData] = useState([]);
 
-  const getRecipe = () => {
-    axios
-      .get(`http://localhost:3001/api/recipes/${recipeId}`)
-      .then((data) => {
-        console.log(data.data);
-        console.log(recipeId);
+  const getRecipe = async () => {
+    try {
+      await axios.get(`http://localhost:3001/api/recipes/${recipeId}`).then((data) => {
         setRecipeData(data.data);
-      })
-      .catch((err) => console.log(err));
+        setIngredientData(data.data.ingredients);
+        setDirectionData(data.data.recipeDirections);
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
-    console.log(recipeId);
     getRecipe();
   }, []);
 
+  const populateIngredientData = () => {
+    return ingredientData.map((ingredient, index) => {
+      return (
+        <div key={index}>
+          <div>{ingredient.ingredientDescrip}</div>
+        </div>
+      );
+    });
+  };
+
+  const populateDirectionData = () => {
+    return directionData.map((direction, index) => {
+      return (
+        <div key={index}>
+          <div>{direction.directionDescrip}</div>
+        </div>
+      );
+    });
+  };
+  
   return (
     <>
-      <div>hey</div>
+      {recipeData ? (
+        <div>
+          <h2>{recipeData.recipeName}</h2>
+          <div>{recipeData.recipeDesc}</div>
+          <h4>Ingredients</h4>
+          <div>{populateIngredientData()}</div>
+          <h4>Directions</h4>
+          <div>{populateDirectionData()}</div>
+        </div>
+      ) : (
+        <h2>Loading</h2>
+      )}
     </>
   );
 }
