9df3f053a1e981c2b081846ef06f0e43a3ffb368
Parker Mathis <parkerjmathis@gmail.com>
2023-05-24 17:48:33 -0700

    Refactor recipe edit page

--- client/src/pages/RecipeEdit.js
+++ client/src/pages/RecipeEdit.js
@@ -11,20 +11,14 @@ import Container from "react-bootstrap/Container";
 
 export default function RecipeCreate() {
   const [recipeData, setRecipeData] = useState({ recipeName: "", recipeDesc: "", ingredients: [], recipeDirections: [] });
-
-  const [recipeName, setRecipeName] = useState("");
-  const [recipeDesc, setRecipeDesc] = useState("");
-  const [ingredientList, setIngredientList] = useState([{ ingredientDescrip: "" }]);
-  const [recipeDirectionList, setRecipeDirectionList] = useState([{ directionDescrip: "" }]);
   const navigate = useNavigate();
   const recipeId = window.location.toString().split("/")[window.location.toString().split("/").length - 1];
 
   const getRecipe = async () => {
     try {
       await axios.get(`http://localhost:3001/api/recipes/${recipeId}`).then((data) => {
-        setRecipeData(data.data);
-        // setRecipeData({ recipeName: recipeData.recipeName, recipeDesc: recipeData.recipeDesc, ingredients: [recipeData.ingredients] });
-        // console.log(recipeData);
+        // console.log(data.data);
+        setRecipeData({ recipeName: data.data.recipeName, recipeDesc: data.data.recipeDesc, ingredients: data.data.ingredients, recipeDirections: data.data.recipeDirections });
       });
     } catch (err) {
       console.log(err);
@@ -35,60 +29,54 @@ export default function RecipeCreate() {
     getRecipe();
   }, []);
 
-  useEffect(() => {
-    setRecipeName(recipeData.recipeName);
-    setRecipeDesc(recipeData.recipeDesc);
-    setIngredientList(recipeData.ingredients);
-    setRecipeDirectionList(recipeData.recipeDirections);
-  }, [recipeData]);
-
   const handleInputChange = (event, index) => {
     const { target } = event;
     const inputType = target.name;
     const inputValue = target.value;
     if (inputType === "recipeName") {
-    //   setRecipeData({ ...recipeData, recipeName: inputValue });
-      setRecipeName(inputValue);
+      setRecipeData({ ...recipeData, recipeName: inputValue });
     } else if (inputType === "recipeDesc") {
-      setRecipeDesc(inputValue);
+      setRecipeData({ ...recipeData, recipeDesc: inputValue });
     } else if (inputType === "ingredientDescrip") {
-      const listIngredient = [...ingredientList];
+      const listIngredient = [...recipeData.ingredients];
       listIngredient[index][inputType] = inputValue;
-      setIngredientList(listIngredient);
+      setRecipeData({ ...recipeData, ingredients: listIngredient });
     } else {
-      const listDirection = [...recipeDirectionList];
+      const listDirection = [...recipeData.recipeDirections];
       listDirection[index][inputType] = inputValue;
-      setRecipeDirectionList(listDirection);
+      setRecipeData({ ...recipeData, recipeDirections: listDirection });
     }
   };
 
   const handleIngredientAdd = () => {
-    setIngredientList([...ingredientList, { ingredientDescrip: "" }]);
+    const ingredientList = [...recipeData.ingredients, { ingredientDescrip: "" }];
+    setRecipeData({ ...recipeData, ingredients: ingredientList });
   };
 
   const handleIngredientRemove = (index) => {
-    const list = [...ingredientList];
+    const list = [...recipeData.ingredients];
     list.splice(index, 1);
-    setIngredientList(list);
+    setRecipeData({ ...recipeData, ingredients: list });
   };
 
   const handleDirectionAdd = () => {
-    setRecipeDirectionList([...recipeDirectionList, { directionDescrip: "" }]);
+    const directionList = [...recipeData.recipeDirections, { directionDescrip: "" }];
+    setRecipeData({ ...recipeData, recipeDirections: directionList });
   };
 
   const handleDirectionRemove = (index) => {
-    const list = [...recipeDirectionList];
+    const list = [...recipeData.recipeDirections];
     list.splice(index, 1);
-    setRecipeDirectionList(list);
+    setRecipeData({ ...recipeData, recipeDirections: list });
   };
 
   const updateRecipe = async () => {
     try {
       await axios.put(`http://localhost:3001/api/recipes/${recipeId}`, {
-        recipeName: recipeName,
-        recipeDesc: recipeDesc,
-        ingredients: ingredientList,
-        recipeDirections: recipeDirectionList,
+        recipeName: recipeData.recipeName,
+        recipeDesc: recipeData.recipeDesc,
+        ingredients: recipeData.ingredients,
+        recipeDirections: recipeData.recipeDirections,
       });
       navigate("/recipelist");
     } catch (err) {
@@ -98,8 +86,6 @@ export default function RecipeCreate() {
   };
 
   const [showAlert, setShowAlert] = useState(false);
-
-  console.log({ ingredientList });
 
   return (
     <>
@@ -114,15 +100,15 @@ export default function RecipeCreate() {
         <Form>
           <Form.Group className="my-1 pt-3 pb-3">
             <h4>Recipe Name</h4>
-            <Form.Control name="recipeName" className="pr-5" value={recipeName || ""} onChange={handleInputChange} />
+            <Form.Control name="recipeName" className="pr-5" value={recipeData.recipeName || ""} onChange={handleInputChange} />
           </Form.Group>
           <Form.Group className="my-1 pb-3">
             <h4>Description</h4>
-            <Form.Control name="recipeDesc" as="textarea" rows={2} placeholder="Description of your recipe" value={recipeDesc || "loading"} onChange={handleInputChange} />
+            <Form.Control name="recipeDesc" as="textarea" rows={2} placeholder="Description of your recipe" value={recipeData.recipeDesc || "loading"} onChange={handleInputChange} />
           </Form.Group>
           <Form.Group className="my-1 pb-3">
             <h4>Ingredients</h4>
-            {ingredientList.map((singleIngredient, index) => (
+            {recipeData.ingredients.map((singleIngredient, index) => (
               <div key={index}>
                 <Row className="my-1">
                   <Col className="">
@@ -134,7 +120,7 @@ export default function RecipeCreate() {
                     />
                   </Col>
 
-                  {ingredientList.length > 1 && (
+                  {recipeData.ingredients.length > 1 && (
                     <Col xs="auto" className="">
                       <Button type="button" style={{ width: "100px" }} onClick={() => handleIngredientRemove(index)}>
                         Remove
@@ -143,7 +129,7 @@ export default function RecipeCreate() {
                   )}
                 </Row>
                 <Row>
-                  {ingredientList.length - 1 === index && (
+                  {recipeData.ingredients.length - 1 === index && (
                     <Col className="my-1">
                       <Button type="button" style={{ width: "150px" }} onClick={handleIngredientAdd}>
                         Add Ingredient
@@ -156,7 +142,7 @@ export default function RecipeCreate() {
           </Form.Group>
           <Form.Group className="my-1 pb-3">
             <h4>Instructions</h4>
-            {recipeDirectionList?.map((singleDirection, index) => (
+            {recipeData.recipeDirections.map((singleDirection, index) => (
               <div key={index}>
                 <Row className="my-1">
                   <Col className="">
@@ -169,7 +155,7 @@ export default function RecipeCreate() {
                       onChange={(event) => handleInputChange(event, index)}
                     />
                   </Col>
-                  {recipeDirectionList.length > 1 && (
+                  {recipeData.recipeDirections.length > 1 && (
                     <Col xs="auto" className="">
                       <Button type="button" style={{ width: "100px" }} onClick={() => handleDirectionRemove(index)}>
                         Remove
@@ -178,7 +164,7 @@ export default function RecipeCreate() {
                   )}
                 </Row>
                 <Row>
-                  {recipeDirectionList.length - 1 === index && (
+                  {recipeData.recipeDirections.length - 1 === index && (
                     <Col className="my-1">
                       <Button type="button" style={{ width: "150px" }} onClick={handleDirectionAdd}>
                         Add Direction
