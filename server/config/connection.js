c51ddc96dbb628cffd450b76db2b9a02fd5446db
Parker Mathis <parkerjmathis@gmail.com>
2023-05-31 16:25:06 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require("mongoose");
 
-connect(process.env.MONGODB_URI || "mongodb://localhost/nomdotcomDB", {
+connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nomdotcomDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
