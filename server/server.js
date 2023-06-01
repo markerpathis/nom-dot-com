915956a3d98299c0354718edea277a0f9b253df3
Parker Mathis <parkerjmathis@gmail.com>
2023-05-31 22:01:29 -0700

    server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
-const path = require("path");
 const express = require("express");
-require("dotenv").config();
-
-const cors = require("cors");
+const path = require("path");
 const db = require("./config/connection");
+require("dotenv").config();
 const routes = require("./controllers");
 
-const PORT = process.env.PORT || 3001;
+// const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, "public")));
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//   });
+// }
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
 }
-
 app.use(routes);
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 db.once("open", () => {
   app.listen(PORT, () => {
