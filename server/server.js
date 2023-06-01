e462b0a3c0a4dc0d57907965f12c51e798da16aa
Parker Mathis <parkerjmathis@gmail.com>
2023-05-31 19:26:23 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,16 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, "public")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
-
-app.get("/", (req, res) => {
+// if (process.env.NODE_ENV === "production") {
+// app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+// }
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 app.use(routes);
 
