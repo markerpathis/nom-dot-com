840f420d0a870a7bbc8d9dd57297bf79f7f0eb33
Parker Mathis <parkerjmathis@gmail.com>
2023-05-31 19:38:17 -0700

    Revert server.js code

--- server/controllers/api/recipeCollectionRoutes.js
+++ server/controllers/api/recipeCollectionRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Recipe, User } = require("../../models");
 
 router.get("/:userId", async (req, res) => {
+  console.log("HEYYYYYYYYY");
   Recipe.find({ author: req.params.userId })
     .then((recipes) => res.json(recipes))
     .catch((err) => res.status(500).json(err));
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const express = require("express");
 require("dotenv").config();
-const mongoose = require("mongoose");
 
 const cors = require("cors");
 const db = require("./config/connection");
@@ -10,43 +9,26 @@ const routes = require("./controllers");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, "public")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-//   });
-// }
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-app.use(routes);
-
-app.get("*", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-// db.once("open", () => {
-//   app.listen(PORT, () => {
-//     console.log(`API server running on port ${PORT}!`);
-//   });
-// });
+app.use(routes);
 
-mongoose.connect(
-  process.env.MONGODB_URI,
-
-  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }
-);
-
-app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
