cd57ec1dc16a7d8978bdffbf484a94a03dc2d4c1
Parker Mathis <parkerjmathis@gmail.com>
2023-06-01 16:03:32 -0700

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const db = require("./config/connection");
 require("dotenv").config();
 const routes = require("./controllers");
 
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+///////// app.use(express.static(path.join(__dirname, "public")));
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "../client/build")));
@@ -21,15 +21,13 @@ app.use(express.static(path.join(__dirname, "public")));
 // }
 
 if (process.env.NODE_ENV === "production") {
-  console.log("PRODUCTIONNNNNNN");
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+app.use(routes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
