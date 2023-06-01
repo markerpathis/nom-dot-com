8f4c5ca651c7cfba8c4227145e7846799d768656
Parker Mathis <parkerjmathis@gmail.com>
2023-06-01 16:42:47 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,8 +25,12 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(routes);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 db.once("open", () => {
