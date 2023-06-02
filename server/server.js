2fcfee7baeac3f306612e5fe1b5e54972fc21f9a
Parker Mathis <parkerjmathis@gmail.com>
2023-06-01 18:16:06 -0700

    Updated icon

--- server/server.js
+++ server/server.js
@@ -11,14 +11,6 @@ const PORT = process.env.PORT || 3001;
 // app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-///////// app.use(express.static(path.join(__dirname, "public")));
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-//   });
-// }
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
